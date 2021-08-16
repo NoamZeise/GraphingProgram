e1f5ae2656dd11ad73b179e718eb77ca7925f9ca
Solar-Chutney <noam4134@gmail.com>
2021-08-16 16:31:20 +0100

    removed unused files

--- resource_manager.cpp
+++
@@ -1,8 +0,0 @@
-#include "resource_manager.h"
-#include "stb_image.h"
-
-static unsigned char* ResourceManager::loadTextureData(const char* path, int* width, int* height, int* nrChannels)
-{
-	unsigned char* data = stbi_load(path, width, height, nrChannels, 0);
-	return data;
-}
